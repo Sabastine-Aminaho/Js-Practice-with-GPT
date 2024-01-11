e0e1ac0947f74fa831fc3eb56f51437d94738e65
Ndukaegho <sabastineaminaho3@gmail.com>
2024-01-11 21:38:36 +0100

    first commit

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ let course = 'Geology';
 var university = 'University of Port Harcourt';
 
 window.alert('Hello ' + name + '!');
-// console.log('you are ' + age + ' year old');
-// console.log('and you graduated from the deparment of ' + course + ' at the ' + university);
+console.log('you are ' + age + ' year old');
+console.log('and you graduated from the deparment of ' + course + ' at the ' + university);
 
-// if (age < 18 ) {
-//     console.log("You are a minor.");
-// } else {
-//         console.log("You are a senior.");
-//       }
+if (age < 18 ) {
+    console.log("You are a minor.");
+} else {
+        console.log("You are a senior.");
+      }
 
 document.querySelector('#practice').innerHTML = 'Hello ' + name + '!';
