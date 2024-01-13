29b699f28aee7d74f50dfd062a47980f8d91757e
Ndukaegho <sabastineaminaho3@gmail.com>
2024-01-13 19:23:31 +0100

    inprogress

---
+++ 02-arrays-and-objects/16-arrays-basics/script.js
@@ -0,0 +1,11 @@
+// Array Literal
+const numbers = [22, 32, 23, 55, 44];
+
+// Array constructor
+
+const fruits = new Array("apple", 'grape', 'mango');
+
+x = numbers[0];
+// fruit = fruits.length
+
+console.log(fruits, x);
