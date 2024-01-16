39cd2332015fbab10f2fabf3d475548800b2fcaf
Ndukaegho <sabastineaminaho3@gmail.com>
2024-01-16 13:59:46 +0100

    number 4

---
+++ 02-arrays-and-objects/08-JSON-intro/script.js
@@ -0,0 +1,13 @@
+const post = {
+    id: 1,
+    title: 'post one',
+    body: 'this is the first post of the year 2024',
+}
+
+// Convert to JSON string
+const str = JSON.stringify(post); // stringify coverts an object tp json file
+
+// Parse JSON
+const obj = JSON.parse(str); //parse convert a JSON string to object
+
+console.log(obj);
